86a029b25896faab8a35d916c5ce0a64d88a29cf
Rohan Coder <sablerohan125@gmail.com>
2026-02-10 18:20:24 +0530

    Token.js

---
+++ models/Token.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = new mongoose.Schema(
+  {
+    student: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+      required: true,
+    },
+    usedAt: {
+      type: Date,
+      default: Date.now,
+    },
+    sessionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Session",
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+tokenSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+tokenSchema.index({ student: 1, expiresAt: 1 });
+
+module.exports = mongoose.model("Token", tokenSchema);
