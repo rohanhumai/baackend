22320144ade78a7d1052543a31d9de5ff5da49fb
Rohan Coder <sablerohan125@gmail.com>
2026-02-10 18:17:51 +0530

    Teacher.js

---
+++ models/Teacher.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const teacherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: 6
+  },
+  department: {
+    type: String,
+    required: [true, 'Department is required'],
+    trim: true
+  },
+  subjects: [{
+    type: String,
+    trim: true
+  }]
+}, {
+  timestamps: true
+});
+
+teacherSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+teacherSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('Teacher', teacherSchema);
