2968345d0d3d8c5a8981e16fca8707bc6557b469
Rohan Coder <sablerohan125@gmail.com>
2026-02-11 21:31:54 +0530

    server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
-      "https://mini-project-frontend1.vercel.app",
+      "https://mini-project-frontend1.vercel.app/",
     ],
     credentials: true,
   }),
